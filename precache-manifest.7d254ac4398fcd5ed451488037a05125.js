079eae8987d1653f193cc317a9741efe65a1e8be
joelraymann <joelraymann@gmail.com>
2020-05-10 16:30:22 +0530

    Updates

---
+++ precache-manifest.7d254ac4398fcd5ed451488037a05125.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "738d197fc0a348ce4e537cfb5aad95da",
+    "url": "/monster-rolodex-app/index.html"
+  },
+  {
+    "revision": "a119d9239d4f14bb7014",
+    "url": "/monster-rolodex-app/static/css/main.38af9f03.chunk.css"
+  },
+  {
+    "revision": "b7025d703d6faac98ebe",
+    "url": "/monster-rolodex-app/static/js/2.a0f84a8f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rolodex-app/static/js/2.a0f84a8f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a119d9239d4f14bb7014",
+    "url": "/monster-rolodex-app/static/js/main.8fa2baec.chunk.js"
+  },
+  {
+    "revision": "0421e18698af01501216",
+    "url": "/monster-rolodex-app/static/js/runtime-main.3502ff71.js"
+  }
+]);
